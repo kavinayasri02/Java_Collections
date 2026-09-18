e938034bebaf4e1533f19727e42c5f64911f728b
Kavinayasri J <kavinayasri02@gmail.com>
2026-09-18 21:31:37 +0530

    Update UpdatingElements.java
    Added functionality to update elements in an ArrayList using user input.

--- UpdatingElements.java
+++ UpdatingElements.java
@@ -12,3 +12,39 @@ public class UpdatingElements {
         System.out.println("The elements in the list after updating are :" + val);
         }
     }
+
+
+//Using  scanner class and integer data type
+
+package ArrayList;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class UpdatingElements {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+   ArrayList<Integer> val = new ArrayList<>();
+   // System.out.println("Enter the number of elements you wanted to add in the list:");
+    int n = sc.nextInt();
+    //System.out.println("Enter the elements you wanted to add in the list:");
+    for(int i=0;i<n;i++){
+        val.add(sc.nextInt());
+    }
+    System.out.println("The elements in the list initially are :");
+    for(int i=0;i<val.size();i++){
+        System.out.println(val.get(i)+" ");
+    }
+    System.out.println("Enter the index of the element you wanted to update in the list:");
+    int index = sc.nextInt();
+    for(int i=0;i<val.size();i++){
+        if(i==index){
+            System.out.println("Enter the new value to be updated :");
+            int newVal = sc.nextInt();
+            val.set(index, newVal);
+            System.out.println("The elements in the list after updation are :");
+            for(int j=0;j<val.size();j++){
+                System.out.println(val.get(j)+" ");
+            }
+        }
+    }
+}
+}
